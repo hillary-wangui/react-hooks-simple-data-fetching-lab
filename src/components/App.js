f3cf2910cf9b279aac2e53485941ed74e04f5303
hillary-wangui <hillary.maina@student.moringaschool.com>
2023-11-23 17:16:08 +0300

    random fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+// create your App component here
+import React from "react"
+import { useState } from "react"
+import { useEffect } from "react"
+function App() {
+    const [randomDogs, setRandomDogs] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => setRandomDogs(data.message))
+        setIsLoading(true)
+    }, [])
+    if(!isLoading) return(<p>Loading...</p>)
+    return(
+        <div>
+            <img src={randomDogs} alt="A Random Dog"></img>
+        </div>
+    )
+}
+
+export default App
